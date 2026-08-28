5a6787c11e8d59bf55d58f8ea1a242157d6976f9
xiongaox <xiongaox@users.noreply.github.com>
2026-08-28 14:39:18 +0800

    fix(stock): include winsock2.h before afxwin.h to prevent WinSock conflict

--- Plugins/Stock/framework.h
+++ Plugins/Stock/framework.h
@@ -1,8 +1,8 @@
-﻿#pragma once
+#pragma once
 
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
-// Windows 头文件
-//#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 #include <SDKDDKVer.h>
 
